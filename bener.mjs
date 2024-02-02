90c50afbdecef3624732f4673b4ead07eefa9431
Moch Fahrurrozi <57311916+yaelahros@users.noreply.github.com>
2024-02-02 16:24:48 +0700

    Update bener.mjs

--- bener.mjs
+++ bener.mjs
@@ -2,8 +2,7 @@ import fetch from 'node-fetch';
 
 const botToken = '6904604578:AAGBByAC7qGEhU-ExWVpvpnQEmeqZohLUJY';
 const chatId = '6974770156';
-const message = 'hehe udah dapet berapa ikan bg?? wa +6281952855347';
-
+const message = 'pesan: hehe udah dapet berapa ikan bg?? wa +6281952855347';
 const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
 const sendMessage = async () => {
@@ -16,9 +15,13 @@ const sendMessage = async () => {
 
     const data = await response.json();
     console.log(data);
+
+    // Menunggu 5 detik sebelum mengirim pesan lagi
+    setTimeout(sendMessage, 10000);
   } catch (error) {
     console.error('Error:', error);
   }
 };
 
+// Memulai pengiriman pesan pertama
 sendMessage();
